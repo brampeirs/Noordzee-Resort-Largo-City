5bc1ee824c97abdd48add5678a4ff5bb2a8d6861
Bram Peirs <peirsbram1@gmail.com>
2017-03-03 17:28:16 +0100

    add sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a.page-scroll').click(function() {
+$('.navbar-collapse ul li a.page-scroll').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
@@ -21,7 +20,7 @@ $('body').scrollspy({
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$('a.page-scroll').bind('click', function(event) {
+$('a.page-scroll').bind('click', function (event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
         scrollTop: ($($anchor.attr('href')).offset().top - 50)
