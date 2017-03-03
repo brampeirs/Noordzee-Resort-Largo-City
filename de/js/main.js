5bb78d7b3852a28915f9ba0ad33eb913dc143c21
Bram Peirs <peirsbram1@gmail.com>
2017-03-03 11:06:05 +0100

    add german version

---
+++ de/js/main.js
@@ -0,0 +1,37 @@
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+/* Andere manier voor bovenstaande */
+/*
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+*/
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 80
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+// Offset for Main Navigation
+$('#mainNav').affix({
+    offset: {
+        top: 100
+    }
+})
